9c203f895c0411e55568541c984aaa23fd01e618
Le Huu Huy <lehuuhuy2902@gmail.com>
2022-03-30 15:18:01 +0700

    config project

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss'
+
+Footer.propTypes = {
+  
+};
+
+function Footer(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss'
+
+Header.propTypes = {
+  
+};
+
+function Header(props) {
+  return (
+    <div>
+      <h3>Hello World</h3>
+    </div>
+  );
+}
+
+export default Header;
