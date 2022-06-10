24ccc1ad1a2f6a945c7bdbd90e78ec515a91afb5
levanhao-it <haole7301@gmail.com>
2022-06-10 13:50:45 +0700

    add helmet dep

--- frontend/src/features/Product/pages/ProductDetailPage/index.jsx
+++ frontend/src/features/Product/pages/ProductDetailPage/index.jsx
@@ -4,68 +4,63 @@ import React from 'react';
 import ProductInfo from '../../components/ProductInfo';
 import ProductRecomend from '../../components/ProductRecomend';
 import ProductSidebar from '../../components/ProductSidebar';
+import { Helmet } from 'react-helmet-async';
 
-ProductDetailPage.propTypes = {
-  
-};
+ProductDetailPage.propTypes = {};
 
 const useStyle = makeStyles((theme) => ({
   root: {
     padding: '60px 40px 0',
     [theme.breakpoints.down('xs')]: {
-      padding: '40px 20px'
+      padding: '40px 20px',
     },
   },
 
   tabsMobile: {
-    marginTop: '20px'
-  }
-}))
+    marginTop: '20px',
+  },
+}));
 
 function ProductDetailPage(props) {
   const classes = useStyle();
   return (
     <div className={classes.root}>
-
+      <Helmet>
+        <title>Product Detail</title>
+      </Helmet>
       {/* Laptop */}
       <Hidden smDown>
-        <Grid container spacing={0} >
+        <Grid container spacing={0}>
           <Grid item xs={12} md={8} lg={8}>
             <ProductInfo />
           </Grid>
 
           <Grid item xs={12} md={4} lg={4}>
-            <ProductSidebar/>
+            <ProductSidebar />
           </Grid>
-          
+
           <Grid item xs={12} md={12} lg={12}>
-          <ProductRecomend />
+            <ProductRecomend />
           </Grid>
         </Grid>
-
       </Hidden>
 
       {/* Tablet - Mobile */}
       <Hidden mdUp>
-      <Grid container spacing={0} >
-          <Grid item xs={12} sm={12} >
-            <ProductSidebar/>
+        <Grid container spacing={0}>
+          <Grid item xs={12} sm={12}>
+            <ProductSidebar />
           </Grid>
 
-          
           <Box className={classes.tabsMobile}>
             <ProductTabsMobile />
           </Box>
-        
-          
+
           <Grid item xs={12} sm={12}>
             <ProductRecomend />
           </Grid>
         </Grid>
       </Hidden>
-     
-
-
     </div>
   );
 }
